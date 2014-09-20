fdd96d65d4c8a993817e921c7f1085f548cff085
Jonathan Hidalgo <dvocryo@hotmail.com>
2014-09-20 18:42:53 -0500

    algunos cambios

--- ticket/Form1.vb
+++ ticket/Form1.vb
@@ -42,6 +42,12 @@ Public Class Form1
             TextBox1.AppendText(NUM_TICKET)
 
 
+            MsgBox("Probando", MsgBoxStyle.Information, "Titulo")
+
+
+            'Provider=SQLNCLI.1;Integrated Security=SSPI;Persist Security Info=True;Initial Catalog=C:\MyBusinessDatabase\MyBusinessPOS2012.mdf;Data Source=JONA_LAP;Use Procedure for Prepare=1;Auto Translate=True;Packet Size=4096;Workstation ID=JONA_LAP;Use Encryption for Data=False;Tag with column collation when possible=False;MARS Connection=False;DataTypeCompatibility=80;Trust Server Certificate=False
+
+
             'ticket
             'Provider=SQLNCLI.1;Password=12345678;Persist Security Info=True;User ID=sa;Initial Catalog=C:\MyBusinessDatabase\MyBusinessPOS2012.mdf;Data Source=TCP:.\SQLEXPRESS,1400;Use Procedure for Prepare=1;Auto Translate=True;Packet Size=4096;Workstation ID=SONOFGOD-PC;Use Encryption for Data=False;Tag with column collation when possible=False;MARS Connection=False;DataTypeCompatibility=0;Trust Server Certificate=False
             '19:
